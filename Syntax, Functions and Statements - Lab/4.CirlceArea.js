380bb1429599c891cc5611adfbccb9b942632eb1
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2022-01-17 13:03:05 +0200

    Add files via upload

---
+++ Syntax, Functions and Statements - Lab/4.CirlceArea.js
@@ -0,0 +1,8 @@
+function solve(input){
+    let inputType = typeof(input);
+    if(inputType == 'number'){
+        console.log((Math.PI * input * input).toFixed(2));
+    }else{
+        console.log(`We can not calculate the circle area, because we receive a ${inputType}.`)
+    }
+}
