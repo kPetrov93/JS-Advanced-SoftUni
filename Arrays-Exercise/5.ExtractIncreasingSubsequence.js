40e8e545383d9a28388edeaafd349588e37e4dcd
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2022-01-21 12:04:44 +0200

    Add files via upload

---
+++ Arrays-Exercise/5.ExtractIncreasingSubsequence.js
@@ -0,0 +1,18 @@
+function extract(arr) {
+
+    arr = arr.map(Number);
+    let result = [arr[0]];
+    let biggestNumber = [arr[0]];
+
+    for (let i = 1; i < arr.length; i++) {
+
+        let currentNumber = arr[i];
+
+        if (currentNumber >= biggestNumber) {
+            result.push(currentNumber);
+            biggestNumber = currentNumber;
+        }
+    }
+
+    return result;
+}
