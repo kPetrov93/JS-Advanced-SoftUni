5cccb35c6f55d08d41efb7d1c847114a4e077015
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2022-01-24 17:51:17 +0200

    Add files via upload

---
+++ Arrays-Exercise/7.SortingNumbers.js
@@ -0,0 +1,13 @@
+function sort(arr){
+    let newArr = [];
+    arr.sort((a,b) => a - b);
+
+    while(arr.length > 0){
+        newArr.push(arr.shift());
+        newArr.push(arr.pop())
+    }
+
+    return newArr;
+}
+
+sort([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
