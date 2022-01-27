11a368f975a182a1e512ec65cd601339bbca83e4
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2022-01-27 13:20:48 +0200

    Add files via upload

---
+++ ObjectAndComposition-Exercise/6.StoreCatalogue.js
@@ -0,0 +1,28 @@
+function solve(input) {
+
+    let result = input.sort((a, b) => a.localeCompare(b));
+
+    let current = [];
+    let char = '';
+
+    for (let i = 0; i < input.length; i++) {
+        current = result[i].split(' : ');
+
+        if (result[i][0] !== char) {
+            console.log(result[i][0]);
+        }
+
+        console.log(`  ${current[0]}: ${current[1]}`);
+
+        char = result[i][0];
+    }
+}
+solve(['Appricot : 20.4',
+'Fridge : 1500',
+'TV : 1499',
+'Deodorant : 10',
+'Boiler : 300',
+'Apple : 1.25',
+'Anti-Bug Spray : 15',
+'T-Shirt : 10']
+)
