65c38dc71e05d93a19458dd615a06437099eeeb0
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2022-01-30 13:29:13 +0200

    Add files via upload

---
+++ DOM-Intro/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let moreText = document.getElementById('text');
+    moreText.style.display = 'inline';
+
+    let removedText = document.getElementById('more');
+    removedText.style.display = 'none';
+}
