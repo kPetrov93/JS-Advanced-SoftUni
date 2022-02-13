1ba07d53db8890e8ff7f0085a2c4c39e003abfa6
Kristian Petrov <83224475+kPetrov93@users.noreply.github.com>
2022-02-13 10:15:46 +0200

    Add files via upload

---
+++ Unit-Test-Exercise/06.Dynamic-Validation/app.js
@@ -0,0 +1,13 @@
+function validate() {
+    document.getElementById('email').addEventListener('change',onChange);
+
+    function onChange(event){
+        const regex = /[a-z]+@[a-z]+\.[a-z]+/gm;
+
+        if(regex.test(event.target.value)){
+            event.target.classList.remove('error');
+        }else{
+            event.target.classList.add('error');
+        }
+    }
+}
